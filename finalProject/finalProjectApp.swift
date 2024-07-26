7461941694c7ebcc1d5332b5cd88e9566d6e654a
Amal Koraishy <amalkoraishy@icloud.com>
2024-07-26 13:25:31 -0400

    Initial Commit

---
+++ finalProject/finalProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  finalProjectApp.swift
+//  finalProject
+//
+//  Created by Amal Koraishy on 7/26/24.
+//
+
+import SwiftUI
+
+@main
+struct finalProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
