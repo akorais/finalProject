39cf850a05fc5c2bd74a950deeb7fd9f6daa96f2
mynameisara <arahv.magine@gmail.com>
2024-08-01 11:35:18 -0400

    Update IntensePunk.swift

--- finalProject/IntensePunk.swift
+++ finalProject/IntensePunk.swift
@@ -16,18 +16,18 @@ struct IntensePunk: View {
                 //Text("Click here for a playlist!")
             VStack {
                     
-                Text("NOSTYLIST- Destroy Lonely")
-                Text("Otaku Hot Girl- Megan Thee Stallion")
-                Text("OKAY- JT")
-                Text("KEHLANI- Jordan Adeetunji")
-                Text("Don Who Leo- Monaleo")
+                Text("Cherry Bomb- The Runaways")
+                Text("One Way Or Another- Blondie")
+                Text("God Save the Queen- Sex Pistols")
+                Text("I Fought the Law- The Clash")
+                Text("Blister In The Sun- Violent Femmes")
                 Text("Got Me Started- Troye Sivan")
-                Text("Not My Fault- Renee Rapp, Megan Thee Stallion")
-                Text("yes, and?- Ariana Grande")
-                Text("Single Ladies(Put a Ring on It)- Beyonce")
-                Text("Jump- Tyla")
+                Text("I Wanna Be Your Dog- The Stooges")
+                Text("Last CAress- Misfits, Glenn Danzig")
+                Text("London Calling- The Clash")
+                Text("Lust For Life- Iggy Pop")
                 
-                Link("Click here for a playlist!",destination: URL (string: "https://open.spotify.com/playlist/37i9dQZF1DX6GwdWRQMQpq?si=bc5981249e164c8d")!)
+                Link("Click here for a playlist!",destination: URL (string: "https://open.spotify.com/playlist/37i9dQZF1DX3LDIBRoaCDQ?si=ddcaee63b6d9468d")!)
             }
             }
     }
